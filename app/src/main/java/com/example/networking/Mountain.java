bc3f42a6776bec1fba15ab32d68343c5f3554ac7
hey <b21tilbe@student.his.se>
2022-05-16 17:43:48 +0200

    Skapat get:ers och set:ers i Mountain.

--- app/src/main/java/com/example/networking/Mountain.java
+++ app/src/main/java/com/example/networking/Mountain.java
@@ -1,4 +1,36 @@
 package com.example.networking;
 
+import com.google.gson.annotations.SerializedName;
+
 public class Mountain {
+
+    String name;
+    String position;
+    @SerializedName("size") private int lenght;
+
+    public String getName(String name){
+        return name;
+    }
+
+    public String getPosition(String position){
+        return position;
+    }
+
+    public String getLength(String length) {
+        return length;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setPosition(String position){
+       this.position = position;
+    }
+
+    public void setLength(String length) {
+        this.length = length;
+    }
+
+
 }
--- app/src/main/java/com/example/networking/MountainAdapter.java
+++ app/src/main/java/com/example/networking/MountainAdapter.java
@@ -23,7 +23,11 @@ public class MountainAdapter extends RecyclerView.Adapter<ViewHolder>{
     public void onBindViewHolder(@NonNull ViewHolder holder, int number) {
         Mountain mountain = Mountains.get(number);
 
-        holder.name.setText(mountain.getName());
+        holder.name.setText(mountain.getName();
+    }
+
+    public void setLocation(String position){
+        this.position = position;
     }
 
     @Override
